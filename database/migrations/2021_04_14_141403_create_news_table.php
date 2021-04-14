816528226696b56863d9ba6dd48ca7e4186dbd08
Joseph Saad <joseph@wiselyinsure.com>
2021-04-14 17:53:01 +0200

    first commit

---
+++ database/migrations/2021_04_14_141403_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("content");
+            $table->integer("rating");
+            $table->dateTime("datetime");
+            $table->foreignId("agency_id")->constrained("agencies")
+                ->onUpdate("cascade")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}

526e4f197ff12cd8cf3caef6c7780450861bee14
Joseph Saad <joseph@wiselyinsure.com>
2021-04-16 10:11:11 +0200

    Api requests to (add or delete) agencies and news

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('agency/create', 'AgencyController@store');
+Route::delete('agency/delete', 'AgencyController@delete');
+
+Route::post('news/create','NewsController@store');
+Route::delete('news/delete', 'NewsController@delete');
+
+//Route::get('news/index', 'NewsController@index');
